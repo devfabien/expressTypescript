8f6f81a78dfb71ff2507face104ef843bc1b5ea5
devfabien <ish001fabi@gmail.com>
2024-04-15 10:01:57 +0200

    feat: create the createUser handler function

--- src/handlers/users.handlers.ts
+++ src/handlers/users.handlers.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from "express";
+import { CreateUserDto } from "../dtos/createUser.dto";
+import { CreateUserQueryParams } from "../types/query-params";
+import { User } from "../types/response";
 
 export function getUsers(request: Request, response: Response) {
   response.send([]);
@@ -6,4 +9,15 @@ export function getUsers(request: Request, response: Response) {
 
 export function getUserById(request: Request, response: Response) {
   response.send({});
+}
+
+export function createUser(
+  request: Request<{}, {}, CreateUserDto, CreateUserQueryParams>,
+  response: Response<User>
+) {
+  return response.status(201).send({
+    id: 1,
+    username: "Devfabien",
+    email: "devfabien@gmial.com",
+  });
 }
