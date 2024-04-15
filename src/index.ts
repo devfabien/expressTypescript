1ff2f5faf25e2016217237152ac1cc9a4ac682ea
devfabien <ish001fabi@gmail.com>
2024-04-15 09:18:08 +0200

    feat: import the usersRouter in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import usersRouter from "./routes/users";
 
 const app = express();
 
 const PORT = 3000;
+
+app.use("/api/users", usersRouter);
 
 app.listen(PORT, () => console.log(`Running on Port ${PORT}`));
