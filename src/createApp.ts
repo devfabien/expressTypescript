32c69c86fe6663cf30f9088d86e58e745a23c729
devfabien <ish001fabi@gmail.com>
2024-04-17 10:38:38 +0200

    feat: create the createApp file

---
+++ src/createApp.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import usersRouter from "./routes/users";
+
+export function createApp() {
+  const app = express();
+  app.use("/api/users", usersRouter);
+  return app;
+}
